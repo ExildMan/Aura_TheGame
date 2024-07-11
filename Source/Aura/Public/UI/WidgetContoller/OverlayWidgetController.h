21c44d1504dbf1f6221449c7e9a22bbefc7b6eca
ExildMan <150153134+ExildMan@users.noreply.github.com>
2024-07-11 13:19:53 +0530

    Overlay Widget Contoller

---
+++ Source/Aura/Public/UI/WidgetContoller/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Subbi GameWorks
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetContoller/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
